442ea506a268a3d3ffccadf20b8c7a54581d0a1b
Giattino <40548@nelnet.net>
2021-06-04 11:48:31 -0400

    completed BeesBeesBees exercise

--- src/Bee.ts
+++ src/Bee.ts
@@ -1,3 +1,15 @@
-class Bee {
+class Bee extends Grub {
+    job:string;
 
+    constructor() {
+        super();  //invokes the constructor from the Super Class
+        this.color = 'yellow' ; // overwrites the property in the Super Class
+        this.age = 5 ;
+        this.job = "keep on growing" ;
+
+    }
+
+    
+    
 };
+
--- src/HoneyMakerBee.ts
+++ src/HoneyMakerBee.ts
@@ -1,1 +1,21 @@
-class HoneyMakerBee extends Bee { };
+class HoneyMakerBee extends Bee {
+    
+        job:string;
+        honeyPot:number ;
+    
+        constructor() {
+            super();  //invokes the constructor from the Super Class
+            this.honeyPot = 0 ; // overwrites the property in the Super Class
+            this.age = 10 ;
+            this.job = "make honey" ;
+        }
+
+        makeHoney(): void {
+            this.honeyPot += 1;   
+          }
+
+        giveHoney(): void {
+            this.honeyPot -= 1;   
+          }
+
+ };
